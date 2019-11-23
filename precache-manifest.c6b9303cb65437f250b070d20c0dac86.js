61a2607633de4d06c924d71b4616afb5b715973a
Alexsavchuk95 <savchukalex57@gmail.com>
2019-11-23 23:03:22 +0300

    Updates

---
+++ precache-manifest.c6b9303cb65437f250b070d20c0dac86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e6165ce2abfa83eaa9c44f9c8bf6a8",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "018570303751c1dc0de7",
+    "url": "/goit-react-hw-02-reader/static/css/main.be9f3268.chunk.css"
+  },
+  {
+    "revision": "883d2550edec79287a59",
+    "url": "/goit-react-hw-02-reader/static/js/2.b8e42aad.chunk.js"
+  },
+  {
+    "revision": "018570303751c1dc0de7",
+    "url": "/goit-react-hw-02-reader/static/js/main.85616d50.chunk.js"
+  },
+  {
+    "revision": "1d7639d31312f9777566",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.f3397c26.js"
+  }
+]);
